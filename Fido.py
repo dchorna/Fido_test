8caa7abe92f44230894d888d11893c7c1d5dcf3a
dchorna <d.chorna@ukma.edu.ua>
2025-09-21 22:10:40 +0300

    Додано readme та зміни до коду

--- Fido.py
+++ Fido.py
@@ -1,14 +1,17 @@
 name = input('Enter file name:')
-fh = open(name)
+try:
+  fh = open(name)
+except FileNotFoundError:
+    print("File with this name not found")
+    exit()
 mylist = list()
-n = 0
 for line in fh:
     line = line.split()
     for words in line:
         if words not in mylist:
             mylist.append(words)
-            n  = n + 1 
+n = len(mylist)
 if n > 1 :
-    x = 2*n//3 + 1
+    x = 2*n//3 - 1
     print(mylist[x])
 else: print("Null")
